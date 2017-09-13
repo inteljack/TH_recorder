36794a0cdf107b190f0dd005bff3fd9c79d2d995
inteljack <inteljack2008@gmail.com>
2017-09-13 09:26:13 +0200

    print out t&h every 3 secs, issue found

--- th_recorder.py
+++ th_recorder.py
@@ -1,6 +1,6 @@
-import Adafruit_DHT as dht
+import Adafruit_DHT
 import time
-import datatime
+import datetime
 
 # Sensor connected to Raspberry Pi 3 pin 4
 DHT_PIN = 4
@@ -9,11 +9,11 @@ DHT_PIN = 4
 DHT_TYPE = Adafruit_DHT.DHT22
 
 # How long to wait between measurements.
-FREQUENCY_SEC = 30
+FREQUENCY_SEC = 3
 
 def th_recorder():
 	humidity, temp = Adafruit_DHT.read(DHT_TYPE, DHT_PIN)
-	print datetime.datetime.now() + 'Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temp, humidity)
+	print datetime.datetime.now(), 'Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temp, humidity)
 
 while True:
 	th_recorder()
